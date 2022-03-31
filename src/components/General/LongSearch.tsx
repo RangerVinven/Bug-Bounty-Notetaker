ab1ed96a4acdd01486e114c9dbd528d63be4d853
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-03-31 21:42:18 +0100

    Added the LongSearch component

---
+++ src/components/General/LongSearch.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const LongSearch = () => {
+    return(
+        <input placeholder="Search" className="pl-1 mt-1 ml-1 border-2 border-black rounded-md w-44"></input>
+    );
+}
+export default LongSearch;
--- src/components/Sidebar/ShortSearch.tsx
+++ src/components/Sidebar/ShortSearch.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Search = () => {
+const ShortSearch = () => {
     return(
         <input placeholder="Search" className="pl-1 mt-1 ml-1 border-2 border-black rounded-md w-32"></input>
     );
 }
-export default Search;
+export default ShortSearch;
